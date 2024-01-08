eca2ab448b4b4d263a2905545358261e55b3ba27
vince-bourgmayer <v.bourgmayer@gmail.com>
2024-01-08 18:48:35 +0100

    feat(damageDisplay): Add hit visual effect base

---
+++ classes/actor/skill.gd
@@ -0,0 +1,18 @@
+class_name Skill extends Node
+
+@export var probability: float
+@export var skill_name: String
+@export var element: Constants.elements
+@export var weapon: Constants.weapons
+@export var power: float #todo or should I use 'Tier'
+var unlocked : bool = false
+var level_condition: int = 0
+# var EFFECT_AREA
+
+func _init(p_name: String, p_elt: Constants.elements, p_weap: Constants.weapons, p_power: float, p_probability: float):
+	skill_name = p_name
+	element = p_elt
+	weapon = p_weap
+	power = p_power
+	probability = p_probability	
+	
--- scenes/GUI_component/battle/visual_effect/damage_display.gd
+++ scenes/GUI_component/battle/visual_effect/damage_display.gd
@@ -3,6 +3,7 @@ class_name DamageDisplayEffect extends Node2D
 
 var label
 var tweenInterval = 0.5
+var hit_texture: TextureRect
 
 func _init(pos: Vector2, dmg: Damage, _tween_interval = 0.3):
 	label = Label.new()
@@ -23,6 +24,9 @@ func _init(pos: Vector2, dmg: Damage, _tween_interval = 0.3):
 		Constants.elements.ICE:
 			label.add_theme_color_override("font_color", Color.DEEP_SKY_BLUE)
 			
+	hit_texture = TextureRect.new()
+	hit_texture.texture = load("res://art/battle/hit_effect.webp")
+	
 	tweenInterval = _tween_interval
 	label.visible = 0
 	position = pos
@@ -32,10 +36,22 @@ func _init(pos: Vector2, dmg: Damage, _tween_interval = 0.3):
 func _enter_tree():
 	#label.anchors_preset = PRESET_CENTER #TODO make it centered
 	add_child(label)
+	add_child(hit_texture)
+	label.position -= Constants.tileSize/2
+	hit_texture.position -= Constants.tileSize/2
+	hit_texture.visible = false
+	var transparency=hit_texture.modulate
+	transparency.a = 0
+	
 	var tween = get_tree().create_tween()
+	tween.tween_interval(0.2)
+	tween.tween_property(hit_texture, "visible", true, 0.01)
+	tween.tween_property(hit_texture, "modulate", transparency, 0.08)
 	tween.tween_interval(tweenInterval)
 	tween.tween_property(label, "visible", true, 0.01)
+
 	tween.tween_property(label, "position", Vector2(label.position.x, label.position.y - 90), 0.65)
+
 	tween.set_trans(Tween.TRANS_EXPO)
 
 	tween.finished.connect(Callable(_on_Tween_finished))
