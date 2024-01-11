86b61439ec1a590a0dd628016682669138a0e4ea
vince-bourgmayer <v.bourgmayer@gmail.com>
2024-01-11 23:01:04 +0100

    feat(game data export): try to export character data in tres files

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -10,12 +10,6 @@ var player = Player.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	init_nav_bar()
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func init_nav_bar():
 	var storyItem = menu_item_scene.instantiate()
@@ -78,3 +72,17 @@ func openTavernMenu():
 		self.remove_child(mainChild)
 		mainChild = load("res://scenes/menu/character_selector_menu/character_list.tscn").instantiate()
 		self.add_child(mainChild)
+
+#func export_characters_data():
+	#const character_data_list_path = "res://gameData/characters_data.tres"
+	#
+	#var high_elf_job = Job.new("the Archer", CreatureStats.new(420, 53, 50, 21, 30), 1, Constants.species.ELF, Constants.weapons.BOW, 0, Constants.rarities.SS, "characters/high_elf/high_elf_job1_icon.webp", "", 0.0, true)
+	#var high_elf = Character.new("High elf", [high_elf_job], 0)
+	#
+	#var goblinSlayer_job = Job.new("as swordman", CreatureStats.new(1027, 94, 98, 50, 45), 1, Constants.species.HUMAN, Constants.weapons.SWORD, 0, Constants.rarities.S, "characters/goblin_slayer/goblin_slayer_job1_icon.webp", "", 0.4, true)
+	#var goblin_slayer = Character.new("Goblin Slayer", [goblinSlayer_job], 0)
+	##characters.append(high_elf)
+	##characters.append(goblin_slayer)
+	#
+	#var result = ResourceSaver.save(high_elf, character_data_list_path)
+	#assert(result == OK)
